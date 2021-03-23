7a6103380096b59917f927eb912472dd258d1ec2
Sam Sycamore <tangled@Sams-MacBook-Pro.local>
2021-03-23 08:40:32 -0700

    cleaining up

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout'
+
+const about = () => {
+  return (
+    <Layout>
+      Coming soon!
+    </Layout>
+  )
+}
+
+export default about
---
+++ pages/shop.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout'
+
+const shop = () => {
+  return (
+    <Layout>
+      Coming soon!
+    </Layout>
+  )
+}
+
+export default shop
